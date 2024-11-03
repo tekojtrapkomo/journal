ee14be4ab5d3df65e7b5fe8558c7b207142b166a
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-02 23:34:22 -0400

    final final

---
+++ src/routes/november-2/+page.svelte
@@ -0,0 +1,120 @@
+<script>
+    import final from '$lib/assets/final.webp';
+
+</script>
+
+<div class="menu">
+    <a href="/">go back</a>
+    <p>november 2, 11:23pm est</p>
+</div>
+<div class="main">
+    <div class="left">
+            <div class="content">
+                1. Hudlaa yrihgui.
+            </div>
+            <div class="content">
+                2.Huuralt, araar tawilt baih yosgui.
+            </div>
+            <div class="content">
+                3. Ymar ch ued shudarga baih yostoi.
+            </div>
+            <div class="content">
+                4. Asuudal garaad irwel zaawal nuur tulah, zugtahgui.
+            </div>
+            <div class="content">
+                5. Hairand hojil, hojigdol gej baihgui.
+            </div>
+            <div class="content">
+                6. Hairtai gej helsen l bol hairtai gesen ug.
+            </div>
+            <div class="content">
+                7. Love is the whole thing, we are only the pieces.
+            </div>
+            <div class="content">
+                I'm feeling so much hope in life, that I will never ever make these mistakes ever again in my life. 😌
+                I think now I can fully move on, and embrace my new journey, and I'll be more open to opportunities. I'm so excited for the future and especially november.
+            </div>
+            </div>
+
+    <div class="right">
+        <img src={final} alt="final quote">    
+    </div>
+</div>
+<style>
+    /* Container */
+    .poem {
+        font-size: 1rem;
+        font-family: 'Times New Roman', serif;
+        font-style: italic;
+        color: black;
+        margin-top: 1rem;
+        background-color: #f9f9f9;
+    }
+    .separator {
+        margin-top: 2rem;
+    }
+    .main {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding-bottom: 1em;
+        font-family: 'reg', sans-serif;
+    }
+
+    .left {
+        width: 50%;
+        display: flex;
+        justify-content: flex-start;
+        padding: 0 2rem;
+        flex-direction: column;
+    }
+    
+    .right {
+        width: 50%;
+        display: flex;
+        justify-content: space-between;
+        padding: 0 2rem;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 4rem;
+    }
+
+    .right img {
+        width: 100%;
+        max-width: 600px;
+    }
+    .content {
+        font-size: 14px;
+        line-height: 1.5;
+        margin-top: 1rem;
+    }
+    blockquote {
+        font-size: 1.5rem;
+        line-height: 1.5;
+        font-family: 'Times New Roman', serif;
+        color: black;
+    }
+    .menu {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding-bottom: 1em;
+        font-family: 'reg', sans-serif;
+        letter-spacing: -4px;
+        padding: 0 2rem 2rem 2rem;
+    }
+
+    .menu a {
+        text-decoration: none;
+        color: black;
+        font-size: 4rem;
+    }
+
+    .menu p {
+        font-size: 4rem;
+        color: blue;
+    }
+
+</style>
