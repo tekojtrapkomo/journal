b78a3201801fcdc7ca25d1d5ee2a0d58f3ccf612
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 06:31:35 -0500

    ggg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,23 @@
     import {onMount} from 'svelte';
     inject({ mode: dev ? 'development' : 'production' });
 
+      // Only load in production
+
+function loadingTwo() {
+  const script = document.createElement('script');
+  script.defer = true;
+  script.dataset.websiteId = "60d97bf7-9627-46fc-81e2-269f0806d7f5";
+
+  script.src = atob("aHR0cHM6Ly9jbG91ZC51bWFtaS5pcy9zY3JpcHQuanM=");
+  document.head.appendChild(script);
+}
     onMount(() => {
         inject();
-            const script = document.createElement('script');
-            script.defer = true;
-            script.src = btoa('aHR0cHM6Ly9jbG91ZC51bWFtaS5pcy9zY3JpcHQuanM='); // Encoded umami URL
-            script.dataset.websiteId = '60d97bf7-9627-46fc-81e2-269f0806d7f5'; // Your ID
-            document.head.appendChild(script);
-        
     })
 </script>
-
+<svelte:head>
+    <script>
+        {loadingTwo.toString()}; loadingTwo();
+    </script>
+</svelte:head>
 <slot/>
