fac6671a89bfa5dcb61c90518e5d7d1edb918359
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 05:53:26 -0500

    mobile fixx

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,5 +10,7 @@
         inject();
     })
 </script>
-
+<svelte:head> 
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="60d97bf7-9627-46fc-81e2-269f0806d7f5"></script>
+</svelte:head>
 <slot/>
