e404f9dbc6428156ad170c3e22884cbbddd3680f
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 06:26:46 -0500

    ff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,12 @@
 
     onMount(() => {
         inject();
-        if (!dev) {
             const script = document.createElement('script');
             script.defer = true;
             script.src = btoa('aHR0cHM6Ly9jbG91ZC51bWFtaS5pcy9zY3JpcHQuanM='); // Encoded umami URL
             script.dataset.websiteId = '60d97bf7-9627-46fc-81e2-269f0806d7f5'; // Your ID
             document.head.appendChild(script);
-        }
+        
     })
 </script>
 
