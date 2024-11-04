5469a2d13be0dc91af3bee59512c96a4451bf010
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 06:34:37 -0500

    ffggr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,24 +4,12 @@
     import { inject } from '@vercel/analytics';
     import {onMount} from 'svelte';
     inject({ mode: dev ? 'development' : 'production' });
-
-      // Only load in production
-
-function loadingTwo() {
-  const script = document.createElement('script');
-  script.defer = true;
-  script.dataset.websiteId = "60d97bf7-9627-46fc-81e2-269f0806d7f5";
-
-  script.src = atob("aHR0cHM6Ly9jbG91ZC51bWFtaS5pcy9zY3JpcHQuanM=");
-  document.head.appendChild(script);
-}
+    const url = 'https://cloud.umami.is/script.js';
     onMount(() => {
         inject();
     })
 </script>
 <svelte:head>
-    <script>
-        {loadingTwo.toString()}; loadingTwo();
-    </script>
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="60d97bf7-9627-46fc-81e2-269f0806d7f5"></script>
 </svelte:head>
 <slot/>
