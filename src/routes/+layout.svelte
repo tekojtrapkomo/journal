521f5e13f7ab653d6fd3a17939e1b69b17b9d434
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 05:56:01 -0500

    bloggy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,15 @@
  
     inject({ mode: dev ? 'development' : 'production' });
     import {onMount} from 'svelte';
+    const loading = !dev;
 
     onMount(() => {
         inject();
     })
 </script>
-<svelte:head> 
+<svelte:head>
+  {#if loading}
     <script defer src="https://cloud.umami.is/script.js" data-website-id="60d97bf7-9627-46fc-81e2-269f0806d7f5"></script>
+  {/if}
 </svelte:head>
 <slot/>
