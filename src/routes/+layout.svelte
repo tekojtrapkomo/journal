77f8186c21901d578299f0a7b5640e393a856e2e
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 06:15:19 -0500

    git

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,19 +9,15 @@
     onMount(() => {
         inject();
     })
-
+    onMount(() => {
+        if (!dev) {
+            const script = document.createElement('script');
+            script.defer = true;
+            script.src = btoa('aHR0cHM6Ly9jbG91ZC51bWFtaS5pcy9zY3JpcHQuanM='); // Encoded umami URL
+            script.dataset.websiteId = '60d97bf7-9627-46fc-81e2-269f0806d7f5'; // Your ID
+            document.head.appendChild(script);
+        }
+    });
 </script>
-<svelte:head>
-    {@html loading ? `
-        <script>
-            (function() {
-                const s = document.createElement('script');
-                s.defer = true;
-                s.src = 'https://cloud.umami.is/script.js';
-                s.setAttribute('data-website-id', '60d97bf7-9627-46fc-81e2-269f0806d7f5');
-                document.head.appendChild(s);
-            })();
-        </script>
-    ` : ''}
-</svelte:head>
+
 <slot/>
