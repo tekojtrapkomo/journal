95d8008a5a9cc8ccc4e61b98a63410ea4153c639
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 11:24:17 -0500

    gggr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,11 @@
     import { inject } from '@vercel/analytics';
     import {onMount} from 'svelte';
     inject({ mode: dev ? 'development' : 'production' });
-    const url = 'https://cloud.umami.is/script.js';
     onMount(() => {
         inject();
     })
 </script>
 <svelte:head>
-    <script defer src="https://cloud.umami.is/script.js" data-website-id="60d97bf7-9627-46fc-81e2-269f0806d7f5"></script>
+    <script defer src="/utils" data-website-id="60d97bf7-9627-46fc-81e2-269f0806d7f5"></script>
 </svelte:head>
 <slot/>
