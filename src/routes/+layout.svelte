7136d8fd68e23771b28b3cd19997df9ac32f252d
tekojtrapkomo <battogtokhportfolio@gmail.com>
2024-11-04 11:28:39 -0500

    nov33

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,21 @@
 <script>
     import '../app.css';
-    import { dev } from '$app/environment';
+    import { dev, browser } from '$app/environment';
     import { inject } from '@vercel/analytics';
     import {onMount} from 'svelte';
     inject({ mode: dev ? 'development' : 'production' });
     onMount(() => {
         inject();
+        if (browser) {
+            const script = document.createElement('script');
+            script.async = true;
+            script.defer = true;
+            script.src = 'https://cloud.umami.is/script.js';
+            script.setAttribute('data-website-id', '60d97bf7-9627-46fc-81e2-269f0806d7f5');
+            document.head.appendChild(script);
+        }
     })
 </script>
 <svelte:head>
-    <script defer src="/utils" data-website-id="60d97bf7-9627-46fc-81e2-269f0806d7f5"></script>
 </svelte:head>
 <slot/>
